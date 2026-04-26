732924b7a99218822086aca976a3efda219ffd82
Junior <juniormosimanenkwe@gmail.com>
2026-04-27 00:00:28 +0200

    fix: update next.config.js to use serverExternalPackages

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ const nextConfig = {
   images: {
     domains: ['axstdjscrdaspqeogole.supabase.co'],
   },
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs'],
-  },
+  serverExternalPackages: ['@prisma/client', 'bcryptjs'],
 }
 module.exports = nextConfig
